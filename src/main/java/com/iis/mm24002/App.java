e05af12d91b5262c47fcdab14f57bcd5c33342f7
Mario12Fz <luismariozamora61@gmail.com>
2025-05-09 17:14:32 -0600

    My first commit

---
+++ src/main/java/com/iis/mm24002/App.java
@@ -0,0 +1,16 @@
+package com.iis.mm24002;
+
+/**
+ * Hello world!
+ */
+
+public class App {
+    public String Hello() {
+        return "Hello World";
+    }
+
+    public static void main(String[] args) {
+        App hw = new App();
+        System.out.println(hw.Hello());
+    }
+}
---
+++ src/test/java/com/iis/mm24002/AppTest.java
@@ -0,0 +1,20 @@
+package com.iis.mm24002;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testHello() 
+    {
+        assertTrue(true);
+    }
+}
